c938f3f83c5f5cef034910b6f605e97ce522013b
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-09-06 01:32:17 +0700

    1

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,60 @@
+package vn.hoidanit.jobhunter.controller;
+
+import java.util.List;
+
+import org.apache.el.stream.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public User createNewUser(@RequestBody User requestUser) {
+        User user = this.userService.handleCreateUser(requestUser);
+        // user.setEmail("hoidanit@gmail.com");
+        // user.setName("dung");
+        // user.setPassword("123456");
+
+        return user;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable("id") long id) {
+
+        return this.userService.fetchUserById(id);
+    }
+
+    @GetMapping("/user")
+    public List<User> getAllUser() {
+
+        return this.userService.fetchAllUser();
+    }
+
+    @PutMapping("/user")
+    public User updateUser(@RequestBody User user) {
+        User ericUser = this.userService.handleUpdateUser(user);
+        return ericUser;
+    }
+}
