93ea077d191c2cfa8cc24d924ee938c08e745aa7
dung11122005 <145743207+dung11122005@users.noreply.github.com>
2024-10-10 19:30:09 +0700

    1

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -48,7 +48,9 @@ public class SecurityConfiguration {
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register",
                 "/storage/**",
                 "/api/v1/email/**",
-
+                "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
         };
 
         http
